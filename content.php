5bcc44f6d0eb2ddde2c5b24ba9340a87742a186e
unknown <ruan.silva149@etec.sp.gov.br>
2024-04-25 20:30:45 -0300

    ajustes barra de navegação

--- content.php
+++ content.php
@@ -2,31 +2,30 @@
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#">Médicos</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+              Menu
             </a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">Sair</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
           </li>
         </ul>
       </div>
@@ -61,7 +60,7 @@
   <nav class="navbar bg-body-tertiary">
       <div class="input-group">
         <span class="input-group-text" id="basic-addon1"></span>
-        <input type="submit" value="Agendar Consulta" class="form-control" aria-describedby="basic-addon1">
+        <input type="submit" value="Agendar Consulta" class="form-control bg-dark border-bottom border-body" data-bs-theme="dark" aria-describedby="basic-addon1" >
       </div>
   </nav>
 </form>
